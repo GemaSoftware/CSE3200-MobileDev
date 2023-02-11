52a306f80fc44023d756ffe3bed82cf327d9b1d7
GemaSoftware <41391455+GemaSoftware@users.noreply.github.com>
2023-02-11 15:47:37 -0500

    Added question strings and question array.

--- CSE3200Lab2/app/src/main/java/com/agrongemajli/cse3200_lab2/MainActivity.kt
+++ CSE3200Lab2/app/src/main/java/com/agrongemajli/cse3200_lab2/MainActivity.kt
@@ -6,6 +6,16 @@ import com.agrongemajli.cse3200_lab2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var activityMainBinding: ActivityMainBinding;
+
+    private val questions = listOf<Question>(
+        Question(R.string.question_1, true),
+        Question(R.string.question_2, false),
+        Question(R.string.question_3, false),
+        Question(R.string.question_4, true),
+        Question(R.string.question_5, true),
+    )
+
+    private var currentIndex: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //Inflate the Layout Binding.
---
+++ CSE3200Lab2/app/src/main/java/com/agrongemajli/cse3200_lab2/Question.kt
@@ -0,0 +1,5 @@
+package com.agrongemajli.cse3200_lab2
+
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val questionResID: Int, val answer: Boolean)
