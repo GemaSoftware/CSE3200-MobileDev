402f9d51876d638552b25d5abd4025dd46cbceb5
GemaSoftware <41391455+GemaSoftware@users.noreply.github.com>
2023-03-21 11:21:52 -0400

    Initial Commit

---
+++ Lab5RadioAG/app/src/main/java/com/agrongemajli/lab5_radio_ag/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.agrongemajli.lab5_radio_ag
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
